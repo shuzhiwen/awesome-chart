49ec59294a8053828627f847c17414d4f21a0aac
shuzhiwen <yuwen4012@gmail.com>
2022-03-15 13:18:01 +0800

    fix: test bug of selector

--- test/helpers/selector.test.ts
+++ test/helpers/selector.test.ts
@@ -18,20 +18,20 @@ test('Selector', () => {
   const canvasContainer = new fabric.Canvas(document.createElement('canvas'))
   const canvasSelector = new Selector('canvas')
 
-  expect(svgSelector.createSubContainer(svgContainer, 'sub')).not.toBeNull()
-  expect(svgSelector.createSubContainer(svgContainer, 'sub')).not.toBeUndefined()
-  expect(canvasSelector.createSubContainer(canvasContainer, 'sub')).not.toBeNull()
-  expect(canvasSelector.createSubContainer(canvasContainer, 'sub')).not.toBeUndefined()
+  expect(svgSelector.createSubcontainer(svgContainer, 'sub')).not.toBeNull()
+  expect(svgSelector.createSubcontainer(svgContainer, 'sub')).not.toBeUndefined()
+  expect(canvasSelector.createSubcontainer(canvasContainer, 'sub')).not.toBeNull()
+  expect(canvasSelector.createSubcontainer(canvasContainer, 'sub')).not.toBeUndefined()
 
   expect(svgSelector.setVisible(svgContainer, true))
   expect(svgSelector.setVisible(svgContainer, false))
   expect(canvasSelector.setVisible(canvasContainer, true))
   expect(canvasSelector.setVisible(canvasContainer, false))
 
-  expect(svgSelector.getFirstChildByClassName(svgContainer, 'sub')).not.toBeNull()
-  expect(svgSelector.getFirstChildByClassName(svgContainer, 'sub')).not.toBeUndefined()
-  expect(canvasSelector.getFirstChildByClassName(canvasContainer, 'sub')).not.toBeNull()
-  expect(canvasSelector.getFirstChildByClassName(canvasContainer, 'sub')).not.toBeUndefined()
+  expect(svgSelector.getSubcontainer(svgContainer, 'sub')).not.toBeNull()
+  expect(svgSelector.getSubcontainer(svgContainer, 'sub')).not.toBeUndefined()
+  expect(canvasSelector.getSubcontainer(canvasContainer, 'sub')).not.toBeNull()
+  expect(canvasSelector.getSubcontainer(canvasContainer, 'sub')).not.toBeUndefined()
 
   expect(svgSelector.remove(svgContainer)).toBeDefined()
   expect(canvasSelector.remove(canvasContainer)).toBeDefined()
