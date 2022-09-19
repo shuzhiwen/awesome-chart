68b32b4c72116be413b9803c6c307fc3a56fd6dc
shuzhiwen <yuwen4012@gmail.com>
2022-09-19 14:19:24 +0800

    feat: optimize global type definition

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,5 +1,7 @@
+import 'fabric/fabric-impl'
 import './global'
 import './module'
+
 export * from './animation'
 export * from './core'
 export * from './data'
@@ -8,3 +10,12 @@ export * from './layer'
 export * from './layout'
 export * from './scale'
 export * from './utils'
+
+declare module 'fabric/fabric-impl' {
+  interface Object {
+    className?: string
+  }
+  interface Group {
+    className?: string
+  }
+}
